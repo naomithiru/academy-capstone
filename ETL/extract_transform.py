90285ca3533d36f9f47d0b89d26809a354de7a97
Naomi Thiru <nimothiru@gmail.com>
2022-02-10 22:57:07 +0000

    Transformed columns

--- ETL/extract_transform.py
+++ ETL/extract_transform.py
@@ -4,14 +4,13 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql.functions import to_timestamp, col
 
 
-
 # #build a spark session
-# #wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.1.2/hadoop-aws-3.1.2.jar
+
 # spark = (SparkSession.builder.config("spark.jars.packages", 'org.apache.hadoop:hadoop-aws:3.1.2')
 #                             .config("fs.s3.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 #                             .config("fs.s3a.aws.credentials.provider", "com.amazonaws.auth.DefaultAWSCredentialsProviderChain").getOrCreate()
 #         )
-# #sc = spark.sparkContext
+
 # df = spark.read.json("s3://dataminded-academy-capstone-resources/raw/open_aq/")
 
 # #write locally to a file
